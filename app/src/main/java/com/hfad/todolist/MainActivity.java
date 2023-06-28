601d91746a229b8f67d8da6445f7e106a077a51a
Tamtam <nastyakrut@gmail.com>
2023-06-28 20:34:38 +0500

    Add NoteActivity

--- app/src/main/java/com/hfad/todolist/MainActivity.java
+++ app/src/main/java/com/hfad/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package com.hfad.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
             notes.add(note);
         }
         showNotes();
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initViews(){
